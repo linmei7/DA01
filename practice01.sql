c6bfad452fb4287243cd125d7fad6abff4cd8d26
linmei7 <124765291+linmei7@users.noreply.github.com>
2023-12-20 20:38:34 +0700

    Ex

---
+++ practice01.sql
@@ -0,0 +1,13 @@
+-- Ex1
+select name from city
+where population > 120000
+and countrycode = 'USA'
+
+-- Ex2
+select * from city
+where countrycode = 'JPN'
+
+-- Ex3
+select city, state from station
+
+-- Ex4
